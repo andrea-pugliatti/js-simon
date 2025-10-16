16bd45267688728d6524f0dc2318ee23d5dd3fc4
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-16 15:28:37 +0200

    Add bonus folder

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,175 @@
+/*
+Simon Says
+
+Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+BONUS:
+Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
+Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
+
+*/
+
+/**
+ * Returns a random number between a min number and a max number.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+const getRandomNumber = (min, max) =>
+	Math.floor(Math.random() * (max - min + 1)) + min;
+
+/**
+ * Generates n random numbers
+ * @param {number} n
+ * @returns {Array}
+ */
+const generateNumberList = (n) => {
+	const list = [];
+	for (let i = 0; i < n; i++) {
+		const randomNumber = getRandomNumber(0, 100);
+		list.push(randomNumber);
+	}
+	return list;
+};
+
+/**
+ * Show the random numbers to the user
+ * @param {Array} list
+ */
+const showRandomNumbers = (list) => {
+	for (let i = 0; i < howManyNumbers; i++) {
+		const thisNumber = list[i];
+		const span = document.createElement("span");
+		span.append(`${thisNumber} `);
+		console.log(span);
+		numbersElement.append(span);
+	}
+};
+
+/**
+ * Creates a list of input elements, prints them and returns the list.
+ * @returns {Array}
+ */
+const buildForm = () => {
+	const list = [];
+	for (let i = 0; i < howManyNumbers; i++) {
+		const thisInput = document.createElement("input");
+		list.push(thisInput);
+	}
+	return list;
+};
+
+/**
+ * Builds and returns a button
+ * @returns {HTMLButtonElement}
+ */
+const buildButton = () => {
+	const buttonElement = document.createElement("button");
+	buttonElement.textContent = "Conferma";
+	return buttonElement;
+};
+
+/**
+ * Receives a list and a button and appends them to inputElement.
+ * @param {Array} list
+ * @param {HTMLButtonElement} list
+ */
+const showForm = (list, button) => {
+	for (let i = 0; i < howManyNumbers; i++) {
+		const thisInput = list[i];
+		inputElement.append(thisInput);
+	}
+	inputElement.append(button);
+};
+
+/**
+ * Builds and shows the final score.
+ * @param {Array} score
+ */
+const showScore = (score) => {
+	const span = document.createElement("span");
+	const letter = score.length === 1 ? "o" : "i";
+	span.append(`Hai trovato ${score.length} numer${letter}!`);
+	if (score.length !== 0) {
+		span.append(` I numeri sono [ `);
+		for (let i = 0; i < score.length; i++) {
+			span.append(`${score[i]} `);
+		}
+		span.append(`]`);
+	}
+	scoreElement.append(span);
+};
+
+/**
+ * Returns the intersection between the two given arrays.
+ * @param {Array} listA
+ * @param {Array} listB
+ * @returns {Array}
+ */
+const compareLists = (listA, listB) => {
+	const intersectionList = [];
+	for (let i = 0; i < howManyNumbers; i++) {
+		const thisValue = listA[i];
+		if (listB.includes(thisValue)) {
+			intersectionList.push(thisValue);
+		}
+	}
+	return intersectionList;
+};
+
+// Select the elements
+const numbersElement = document.getElementById("numbers");
+const inputElement = document.getElementById("input");
+const scoreElement = document.getElementById("score");
+
+// Global values
+const gameTimer = 3000;
+const howManyNumbers = 5;
+
+// Generate 5 random numbers
+const randomNumbersList = generateNumberList(howManyNumbers);
+
+// Print the numbers on screen
+showRandomNumbers(randomNumbersList);
+
+// Build inputs
+const inputElementList = buildForm();
+const buttonElement = buildButton();
+
+// Add timer
+setTimeout(() => {
+	// When the timer runs out
+	// Remove the numbers
+	numbersElement.style.display = "none";
+	// Add the input boxes/form
+	showForm(inputElementList, buttonElement);
+	inputElement.style.display = "block";
+}, gameTimer);
+
+// On button click take the numbers from the inputs and save them
+buttonElement.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	// Retrieve values
+	const valuesList = [];
+	for (let i = 0; i < howManyNumbers; i++) {
+		const thisValue = Number(inputElementList[i].value);
+		valuesList.push(thisValue);
+	}
+
+	// Compare the numbers entered by the user with the generated ones
+	const score = compareLists(valuesList, randomNumbersList);
+	// Build and show the score in the score element
+	showScore(score);
+
+	// Hide form
+	inputElement.style.display = "none";
+	// Print on screen the score and which numbers were found
+	scoreElement.style.display = "block";
+});
